bbf4ce5241c25e004673638c0e06a8d148f804ea
Ivan Kalagin <ivan.kalagin@inplat.ru>
2022-02-28 16:42:52 +0300

    Init

---
+++ mars-rover-kata/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mars-rover-kata-2
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ mars-rover-kata/src/mars_rover_kata_2/core.clj
@@ -0,0 +1,38 @@
+;; (ns mars-rover-kata-2.core
+;;   (:gen-class))
+
+;; (defn -main
+;;   "I don't do a whole lot ... yet."
+;;   [& args]
+;;   (println "Hello, World!"))
+
+(def rover {:position {:x 0 :y 0} :orientation "North"})
+(def planet {:width 5 :height 5})
+
+(def rover-atom (atom rover))
+
+(defn rover-turn-right [] 
+  (swap! rover-atom assoc :orientation 
+         (cond
+           (= (get @rover-atom :orientation) "North") "Est"
+           (= (get @rover-atom :orientation) "Est") "South"
+           (= (get @rover-atom :orientation) "South") "West"
+           (= (get @rover-atom :orientation) "West") "North"
+           :else (get @rover-atom :orientation))))
+
+(defn rover-turn-left []
+  (swap! rover-atom assoc :orientation 
+         (cond
+           (= (get @rover-atom :orientation) "North") "West"
+           (= (get @rover-atom :orientation) "West") "South"
+           (= (get @rover-atom :orientation) "South") "Est"
+           (= (get @rover-atom :orientation) "Est") "North"
+           :else (get @rover-atom :orientation))))
+
+(defn rover-move-forward []
+  (cond
+    (= (get @rover-atom :orientation) "North") (swap! rover-atom update-in [:position :y] inc)
+    (= (get @rover-atom :orientation) "West") (swap! rover-atom update-in [:position :x] dec)
+    (= (get @rover-atom :orientation) "South") (swap! rover-atom update-in [:position :y] dec)
+    (= (get @rover-atom :orientation) "Est") (swap! rover-atom update-in [:position :x] inc)
+    :else (get @rover-atom :position)))
