c9bbcb95e3d2eb8ff610e8f00a5f3283631cdca0
Ivan Kalagin <ivan.kalagin@inplat.ru>
2022-03-04 12:58:42 +0300

    Add backward, handle planet w/h, obstacles handling

--- mars-rover-kata/src/mars_rover_kata_2/core.clj
+++ mars-rover-kata/src/mars_rover_kata_2/core.clj
@@ -1,11 +1,9 @@
 (ns mars-rover-kata-2.core
   (:gen-class))
 
-;; Pure part
-
-(def ->right {:north :est, :est :south, :south :west, :west :north})
-(def ->left {:north :west, :west :south, :south :est, :est :north})
-(def ->opposite {:north :south, :south :north, :est :west, :west :est})
+(def ->right {:north :east, :east :south, :south :west, :west :north})
+(def ->left {:north :west, :west :south, :south :east, :east :north})
+(def ->opposite {:north :south, :south :north, :east :west, :west :east})
 
 (defn turn-left [rover] (update rover :orientation ->left))
 (defn turn-right [rover] (update rover :orientation ->right))
@@ -19,92 +17,49 @@
     :west (update-in rover [:position :x] dec)
     rover))
 
+(defn move-backward [rover]
+  (-> rover turn-opposite move-forward))
+
 (def commands
   {:move-forward move-forward
+   :move-backward move-backward
    :turn-left turn-left
    :turn-right turn-right
    :turn-opposite turn-opposite})
-
-(defn move [rover command]
-  (let [cmd-fn (commands command)]
-    (cmd-fn rover)))
 
 (def initial-rover
   {:position {:x 0 :y 0}
    :orientation :north})
 
-;; Let's quick check what is going on.
-(comment
-  (turn-left initial-rover) ;; _ <- place cursor into this place and evaluate the form
-  (turn-right initial-rover)
-  (turn-opposite initial-rover)
-  (-> initial-rover
-      move-forward
-      move-forward
-      turn-left
-      move-forward)
-  (move initial-rover :turn-left)
-  )
+(def obstacles [{:x 2 :y 2} {:x 3 :y 3}])
+
+(def planet {:width 10 :height 10})
+
+(defn rover-can-move? [rover planet]
+  (let [roverX (get-in rover [:position :x])
+        roverY (get-in rover [:position :y])
+        planetW (:width planet)
+        planetH (:height planet)] (and (>= roverX 0) (>= roverY 0) (<= roverX planetW) (<= roverY planetH))))
+
+(defn rover-hit-obstacle? [rover obstacles]
+  (let [roverX (get-in rover [:position :x])
+        roverY (get-in rover [:position :y])]
+    (true? (some #(and (= roverX (get-in % [:x])) (= roverY (get-in % [:y]))) obstacles))))
+
+(defn move [rover command planet obstacles]
+  (let [cmd-fn (commands command)
+        next-rover-move (cmd-fn rover)]
+    (if (and (rover-can-move? next-rover-move planet) (not (rover-hit-obstacle? next-rover-move obstacles)))
+      next-rover-move
+      rover)))
+
 
 ;; Non pure part of the app.
-;; In the best scenario this part is about 5-10% of the all application code.
 
 (def a-rover (atom initial-rover))
 
-(defn move! [command]
-  (swap! a-rover move command))
+(defn move! [command planet obstacles]
+  (swap! a-rover move command planet obstacles))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
-
-(def rover )
-(def planet {:width 5 :height 5})
-
-(def rover-atom (atom rover))
-
-(defn rover-turn-right []
-  (swap! rover-atom assoc :orientation
-         (cond
-           (= (get @rover-atom :orientation) "North") "Est"
-           (= (get @rover-atom :orientation) "Est") "South"
-           (= (get @rover-atom :orientation) "South") "West"
-           (= (get @rover-atom :orientation) "West") "North"
-           :else (get @rover-atom :orientation))))
-
-(defn rover-turn-left []
-  (swap! rover-atom assoc :orientation
-         (cond
-           (= (get @rover-atom :orientation) "North") "West"
-           (= (get @rover-atom :orientation) "West") "South"
-           (= (get @rover-atom :orientation) "South") "Est"
-           (= (get @rover-atom :orientation) "Est") "North"
-           :else (get @rover-atom :orientation))))
-
-(defn rover-turn-opposite []
-  (swap! rover-atom assoc :orientation
-         (cond
-           (= (get @rover-atom :orientation) "North") "South"
-           (= (get @rover-atom :orientation) "South") "North"
-           (= (get @rover-atom :orientation) "Est") "West"
-           (= (get @rover-atom :orientation) "West") "Est"
-           :else (get @rover-atom :orientation))))
-
-(defn rover-move-forward []
-  (cond
-    (= (get @rover-atom :orientation) "North") (swap! rover-atom update-in [:position :y] inc)
-    (= (get @rover-atom :orientation) "West") (swap! rover-atom update-in [:position :x] dec)
-    (= (get @rover-atom :orientation) "South") (swap! rover-atom update-in [:position :y] dec)
-    (= (get @rover-atom :orientation) "Est") (swap! rover-atom update-in [:position :x] inc)
-    :else (get @rover-atom :position)))
-
-(defn rover-move-backward []
-  (rover-turn-opposite)
-  (rover-move-forward)
-)
-
-(rover-move-forward)
-(rover-move-backward)
+(move! :turn-right planet obstacles)
+(move! :move-forward planet obstacles)
