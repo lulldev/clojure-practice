cf33650717f659c8c46502f6be724225af5e1a70
Ivan Shvetsov <shvetsov.ivan.v@gmail.com>
2022-03-05 12:01:57 +0500

    Do clojure way

--- mars-rover-kata/src/mars_rover_kata_2/core.clj
+++ mars-rover-kata/src/mars_rover_kata_2/core.clj
@@ -20,70 +20,80 @@
 (defn move-backward [rover]
   (-> rover turn-opposite move-forward))
 
-(defn meet-obstacle [rover]
-  (update-in rover [:is-meet-obstacle] (fn [_] true)))
-
-(def commands
-  {:move-forward move-forward
+(def command-handlers
+  {:move-forward  move-forward
    :move-backward move-backward
-   :turn-left turn-left
-   :turn-right turn-right
+   :turn-left     turn-left
+   :turn-right    turn-right
    :turn-opposite turn-opposite})
 
+(defn rover-hit-planet-edge? [rover planet]
+  (let [x (get-in rover [:position :x])
+        y (get-in rover [:position :y])
+        planet-w (:width planet)
+        planet-h (:height planet)]
+    (or (neg? x) (neg? y) (> x planet-w) (> y planet-h))))
+(rover-hit-planet-edge? initial-rover planet)
+
+(defn rover-hit-obstacle? [rover obstacles]
+  (boolean (some (partial = (:position rover)) obstacles)))
+
+(defn move [rover command planet obstacles]
+  (let [cmd-fn (command-handlers command)
+        next-rover (cmd-fn rover)]
+    (cond
+      (rover-hit-obstacle? next-rover obstacles) {:status :obstacle :rover rover}
+      (rover-hit-planet-edge? rover planet) {:status :planet-edge :rover rover}
+      :else  {:status :ok :rover next-rover})))
+
+
+;; (defn rover-travel! [travel-route planet obstacles]
+;;   (loop [i 0]
+;;   (when (< i (count travel-route))
+;;       (if (true? (get-in @a-rover [:is-meet-obstacle]))
+;;         (print "Rover meet obstacle, stop!")
+;;         (do
+;;              (move! (nth travel-route i) planet obstacles)
+;;              (println "Rover go to" (get-in @a-rover [:position]))
+;;              (recur (inc i))
+;;              )))))
+
+(defn travel [rover commands obstacles planet]
+  (loop [r rover
+         [cmd & other-cmds] commands
+         result []]
+    (if cmd
+      (let [move-result (move r cmd planet obstacles)]
+        (recur (:rover move-result) other-cmds (conj result move-result)))
+      result)))
+
 (def initial-rover
-  {:position {:x 0 :y 0}
-   :orientation :north
-   :is-meet-obstacle false})
+  {:position    {:x 0 :y 0}
+   :orientation :north})
 
 (def obstacles [{:x 2 :y 2} {:x 3 :y 3}])
 
 (def planet {:width 10 :height 10})
 
-(defn rover-can-move? [rover planet]
-  (let [roverX (get-in rover [:position :x])
-        roverY (get-in rover [:position :y])
-        planetW (:width planet)
-        planetH (:height planet)] (and (>= roverX 0) (>= roverY 0) (<= roverX planetW) (<= roverY planetH))))
+(def cmds
+  [:move-forward
+   :move-forward
+   :move-forward
+   :turn-right
+   :move-forward
+   :move-forward
+   :move-forward
+   :move-backward])
 
-(defn rover-hit-obstacle? [rover obstacles]
-  (let [roverX (get-in rover [:position :x])
-        roverY (get-in rover [:position :y])]
-    (true? (some #(and (= roverX (get-in % [:x])) (= roverY (get-in % [:y]))) obstacles))))
+(defn main []
+  (let [statuses (travel initial-rover cmds obstacles planet)]
+    (doseq [s statuses]
+      (let [msg (condp = (:status s)
+                  :obstacle "Rover met obstacle, stop!"
+                  :planet-edge "Rover is on planet edge, stop!"
+                  :ok (str "Rover go to " (-> s :rover :position)))]
+        (println msg)))))
 
-(defn move [rover command planet obstacles]
-  (let [cmd-fn (commands command)
-        next-rover-move (cmd-fn rover)]
-    (cond
-      (rover-hit-obstacle? next-rover-move obstacles) (meet-obstacle rover)
-      (rover-can-move? next-rover-move planet) next-rover-move
-      :else rover)))
-
-
-;; Non pure part of the app.
-
-(def a-rover (atom initial-rover))
-
-(defn move! [command planet obstacles]
-  (swap! a-rover move command planet obstacles))
-
-(def travel-route [:move-forward
-                   :move-forward
-                   :move-forward
-                   :turn-right
-                   :move-forward
-                   :move-forward
-                   :move-forward
-                   :move-backward])
-
-(defn rover-travel! [travel-route planet obstacles]
-  (loop [i 0]
-  (when (< i (count travel-route))
-      (if (true? (get-in @a-rover [:is-meet-obstacle]))
-        (print "Rover meet obstacle, stop!")
-        (do 
-             (move! (nth travel-route i) planet obstacles)
-             (println "Rover go to" (get-in @a-rover [:position]))
-             (recur (inc i))
-        )))))
-
-(rover-travel! travel-route planet obstacles)
+(comment
+  (main)
+  )
